9b8b0b66e545b58f70c81e9a7c39d3078a2bce64
Lazy <ricardog.n@hotmail.com>
2025-01-05 15:36:17 -0300

    WE started the visual part

--- index.js
+++ index.js
@@ -4,9 +4,40 @@ async function getPokemon(pokemonName) {
 
     let pokemonImage = data["sprites"]["front_default"];
     let pokemonTypes = data["types"];
-    let secondType = pokemonTypes[1]
-    console.log(secondType);
+    let firstType = pokemonTypes[0]["type"]["name"];
+    let secondType = "none";
+
+    if (pokemonTypes.length > 1) {
+        secondType = pokemonTypes[1]["type"]["name"];
+    }
+
+    let pokemon = {
+        name: pokemonName,
+        image: pokemonImage,
+        types: [
+            firstType,
+            secondType
+        ],
+        talk: () => {
+            let midPoint = Math.ceil(pokemonName.length / 2);
+            let halfName = pokemonName.slice(0, midPoint)
+            console.log(halfName + " " + halfName);
+        }
+    }
+
+    return pokemon;
 }
 
-getPokemon("charizard");
+async function getAllPokemons() {
+    let response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=150");
+    let data = await response.json();
 
+    console.log(data);
+}
+
+async function getMyPokemon(pokemonName) {
+    let myPokemon = await getPokemon(pokemonName);
+    myPokemon.talk();
+}
+
+getMyPokemon("squirtle");
---
+++ second.js
@@ -0,0 +1,14 @@
+let myObject = {
+    prop1: "valor",
+    prop2: 23,
+    method: () => {
+        console.log("sdasdas");
+    }
+}
+
+myObject.prop1 = "dsadsadsad";
+myObject["prop2"];
+myObject.method;
+myObject["method"];
+
+myObject.prop3 = "tessssst2";
