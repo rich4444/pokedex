62e7e53c4b16c3ad80b268d9950555b0d4a9d187
Lazy <ricardog.n@hotmail.com>
2025-01-26 13:11:57 -0300

    Lastest changes

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+let pokemonOptionsContainer = document.querySelector(".pokemonOptions");
+let pokemonNameInput = document.querySelector(".pokemonNameInput");
+let pokemonImage = document.querySelector(".pokemonPicture img");
+
+pokemonNameInput.addEventListener("input", async () => {
+    let allPokemons = await getAllPokemons();
+    createPokemonsLists(allPokemons, pokemonNameInput.value);
+})
+
+
 async function getPokemon(pokemonName) {
     let response = await fetch("https://pokeapi.co/api/v2/pokemon/" + pokemonName);
     let data = await response.json();
@@ -32,7 +42,7 @@ async function getAllPokemons() {
     let response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=150");
     let data = await response.json();
 
-    console.log(data);
+    return data.results;
 }
 
 async function getMyPokemon(pokemonName) {
@@ -40,4 +50,48 @@ async function getMyPokemon(pokemonName) {
     myPokemon.talk();
 }
 
-getMyPokemon("squirtle");
+// getMyPokemon("squirtle");
+
+function createPokemonsLists(pokemons, filter) {
+    pokemonOptionsContainer.innerHTML = "";
+
+    for (let i = 0; i < pokemons.length; i++) {
+        if (pokemons[i].name.includes(filter) || filter == undefined) {
+            let pokemonOption = document.createElement("li");
+            pokemonOption.classList.add("pokemonOption");
+            pokemonOption.innerHTML = pokemons[i].name;
+
+            pokemonOption.addEventListener("click", async () => {
+                let pokemonName = pokemons[i].name;
+
+                pokemonNameInput.value = pokemonName;
+                createPokemonsLists(pokemons, pokemonName);
+
+                let pokemonData = await getPokemon(pokemonName);
+
+                updatePokemon(pokemonData);
+            })
+
+            pokemonOptionsContainer.appendChild(pokemonOption);
+        }
+    }
+}
+
+function updatePokemon(pokemonData) {
+    /*
+    Actualizar la imagen
+    destacar el tipo de pokemon
+    actualizar la descripcion
+    */
+
+    pokemonImage.src = pokemonData.image;
+}
+
+
+async function init() {
+    let allPokemons = await getAllPokemons();
+    createPokemonsLists(allPokemons);
+}
+
+
+init();
